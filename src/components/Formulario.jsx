78574ec027ea2fdd488657d5c5e32ea8198f6c8c
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-01-29 21:22:40 -0300

    agregado todos los useState

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import { Card, Form, Container, Row, Col, Button } from "react-bootstrap";
 
 const Formulario = () => {
+    
+    const [nombreMascota, SetNombreMascota] = useState ("");
+    const [nombreDuenio, SetnombreDuenio] = useState ("");
+    const [fecha, SetFecha] = useState ("");
+    const [hora, SetHora] = useState ("");
+    const [sintomas, SetSintomas] = useState ("");
+
     return (
         <> 
         <Container className="mt-5">
@@ -10,33 +18,41 @@ const Formulario = () => {
 
       <Form.Group className="m-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Nombre Mascota</Form.Label>
-        <Form.Control type="text" placeholder="name@example.com" />
+        <Form.Control type="text" placeholder="Jan" 
+        required onChange={(e)=> SetNombreMascota(e.target.value)} 
+        value={nombreMascota}/>
+         
       </Form.Group>
 
       <Form.Group className="m-3 " controlId="exampleForm.ControlInput1">
         <Form.Label>Nombre Dueño</Form.Label>
-        <Form.Control type="text" placeholder="name@example.com" />
+        <Form.Control type="text" placeholder="Adrian"
+        required onChange={(e)=> SetnombreDuenio(e.target.value)} 
+        value={nombreDuenio}/>
       </Form.Group>
 <Row>
 <Col sm={4}>
       <Form.Group className="m-3 " controlId="exampleForm.ControlInput1">
         <Form.Label>Fecha</Form.Label>
-        <Form.Control type="date" placeholder="name@example.com" />
+        <Form.Control type="date" required onChange={(e)=> SetFecha(e.target.value) } 
+        value={fecha}/>
       </Form.Group>
       </Col>
       <Col sm={4}>
       <Form.Group className="m-3 " controlId="exampleForm.ControlInput1">
         <Form.Label>Hora</Form.Label>
-        <Form.Control type="time" placeholder="name@example.com" />
+        <Form.Control type="time" required onChange={(e)=> SetHora(e.target.value)} 
+        value={hora}/>
       </Form.Group>
       </Col>
       </Row>
       <Form.Group className="m-3 " controlId="exampleForm.ControlTextarea1">
         <Form.Label>Sintomas</Form.Label>
-        <Form.Control as="textarea" rows={3} resize="none" />
+        <Form.Control as="textarea" rows={3} resize="none" placeholder="Escriba los Sintomas de la Mascota" required onChange={(e)=> SetSintomas(e.target.value)} 
+        value={sintomas}/>
       </Form.Group>
       
-      <Button className="m-3">Crear cita Nueva</Button>
+      <Button className="m-3" type="submit">Crear cita Nueva</Button>
     </Form>
       </Card>
       </Container>
